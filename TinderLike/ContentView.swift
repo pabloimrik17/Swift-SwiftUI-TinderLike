4e0a5e28ff21235bf26423de1eba1c9aa2ed30a3
Pablo Fernandez Guerra <pabloimrik17@gmail.com>
2020-03-05 22:58:25 +0100

    Project init

---
+++ TinderLike/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TinderLike
+//
+//  Created by Pablo Fernandez Guerra on 05/03/2020.
+//  Copyright © 2020 Pablo Fernandez Guerra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
